cc2b56d1f4721bf988221233f84c94aca4809a30
Jose Luis Fernandez <fernandezjl25@gmail.com>
2016-11-10 11:49:21 +0100

    0921 1. fijar hora

---
+++ ClockDisplay.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private NumberDisplay hora;
+    private NumberDisplay minuto;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        // initialise instance variables
+        hora = new NumberDisplay(24);
+        minuto = new NumberDisplay(60);
+    }
+
+    
+    public void fijarHora(int nuevaHora, int nuevoMinuto)
+    {
+        hora.setValue(nuevaHora);
+        minuto.setValue(nuevoMinuto);
+    }
+}
